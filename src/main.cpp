cee1cd3ee734ed458cbe8350068109709639ec5c
triqadafi <mail@triqadafi.com>
2017-12-30 15:17:11 +0700

    last successful build

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,38 @@
 #include <SoftwareSerial.h>
 #include <PinChangeInterrupt.h>
 
-#define PIN_RX 0
-#define PIN_TX 1
+#define PIN_RX 1
+#define PIN_TX 0
 #define PIN_PROXIMITY 2
 #define PIN_INT_A 3
 #define PIN_INT_B 4
 
-SoftwareSerial blueToothSerial(0, 1); // RX, TX
-
-volatile bool PIN_CHANGE = false;
-
-void fi_pin_change(){
-  PIN_CHANGE = true;
-}
+SoftwareSerial blueToothSerial(PIN_RX, PIN_TX); // RX, TX
 
 void setup() {
-    // put your setup code here, to run once:
-
-  //pinMode(PIN_PROXIMITY, INPUT_PULLUP);
-  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(PIN_PROXIMITY), fi_pin_change, FALLING);
+  // Setup the IO
+  pinMode(PIN_PROXIMITY, INPUT_PULLUP);
   pinMode(PIN_INT_A,OUTPUT); digitalWrite(PIN_INT_A, LOW);
   pinMode(PIN_INT_B,OUTPUT); digitalWrite(PIN_INT_A, LOW);
 
+  // Start the Serial
   blueToothSerial.begin(38400);
-  digitalWrite(PIN_INT_A, HIGH);
+
+  // INIT
   blueToothSerial.println("3913;39;1;");
   delay(1000);
-  digitalWrite(PIN_INT_B, HIGH);
   blueToothSerial.println("3913;39;2;");
+
+  digitalWrite(PIN_INT_A, HIGH);
+  digitalWrite(PIN_INT_B, HIGH);
+  delay(1000);
   digitalWrite(PIN_INT_A, LOW);
   digitalWrite(PIN_INT_B, LOW);
 }
 
 void loop() {
-  if(PIN_CHANGE == true){
+  if(digitalRead(PIN_PROXIMITY) == LOW){
     blueToothSerial.println("3913;39;2;");
-    delay(1000);
-    PIN_CHANGE = false;
+    delay(3000);
   }
 }
